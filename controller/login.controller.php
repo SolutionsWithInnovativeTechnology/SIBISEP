2b8decaed9d71aa21dc6a0d51693393f7bffc673
Saúl <sauln559@gmail.com>
2024-10-21 00:06:28 -0500

    rutas dinamicas

--- controller/login.controller.php
+++ controller/login.controller.php
@@ -6,5 +6,13 @@
     $_SESSION['user'] = $user;
     $_SESSION['ur'] = 611;
     $_SESSION['area'] = "Area de Ejemplo";
-    header("Location: ../index");
+
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https" : "http";
+    $host = $_SERVER['HTTP_HOST'];
+    $path = dirname($_SERVER['SCRIPT_NAME'], 2);
+    $page = "";
+    $url = "$protocol://$host$path/$page";
+    
+    header("Location:$url");
+    die();
 ?>
--- controller/logout.controller.php
+++ controller/logout.controller.php
@@ -1,5 +1,11 @@
 <?php
     session_start();
     session_destroy();
-    header("Location: ../index");
+
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https" : "http";
+    $host = $_SERVER['HTTP_HOST'];
+    $path = dirname($_SERVER['SCRIPT_NAME'], 2);
+    $page = "";
+    $url = "$protocol://$host$path/$page";
+    header("Location:$url");
 ?>
